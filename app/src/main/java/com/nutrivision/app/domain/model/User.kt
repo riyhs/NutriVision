2abc9a50edd3f62b285d775db169fb3472e67004
Riyaldi Hasan <riy.2923@gmail.com>
2025-06-29 17:09:17 +0700

    fix: change gender to enum

--- app/src/main/java/com/nutrivision/app/domain/model/User.kt
+++ app/src/main/java/com/nutrivision/app/domain/model/User.kt
@@ -1,11 +1,13 @@
 package com.nutrivision.app.domain.model
 
+import com.nutrivision.app.utils.Gender
+
 data class User (
     val uid: String,
     val displayName: String,
     val email: String,
     val age: Int,
-    val gender: String,
+    val gender: Gender? = null,
     val height: Float,
     val weight: Float,
     val photoUrl: String? = null
